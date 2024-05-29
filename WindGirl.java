e7ac7399cb5e307e98139101abe777347c6b75fe
EmChen06 <emileechen95@gmail.com>
2024-05-29 12:40:44 -0400

    WindGirl

---
+++ WindGirl.java
@@ -0,0 +1,12 @@
+import java.awt.image.BufferedImage;
+
+public class WindGirl extends Character{
+
+    WindGirl(int startX, int startY, BufferedImage image, boolean jump) {
+        super(startX, startY, image, jump);
+    }
+
+    void doubleJump() {
+    }
+
+}
