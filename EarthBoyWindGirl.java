19aff51f9d4b78e5e19125572f91724fab529e58
Sheng-777 <hanshengliu2024@gmail.com>
2024-05-29 12:31:21 -0400

    Add Platform Class, implement platform drawing

--- EarthBoyWindGirl.java
+++ EarthBoyWindGirl.java
@@ -6,12 +6,54 @@ import java.util.*;
 
 public class EarthBoyWindGirl extends JFrame {
 
-    JPanel p;
+    DrawingPanel p;
+    int WIDTH = 1000;
+    int HEIGHT = 600;
+    ArrayList<Platform> platforms = new ArrayList<>();
+    
+    
+    EarthBoyWindGirl(){
+        this.setSize(new Dimension(WIDTH,HEIGHT));
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        p = new DrawingPanel();
+        this.add(p);
+
+        
+        platforms.add(new Platform(50, 50, 100, 20, null));
+
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+
+    private class DrawingPanel extends JPanel{
+
+        DrawingPanel(){
+            this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2 = (Graphics2D)g;
+			//turn on antialiasing
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+
+
+            for (Platform platform : platforms) {
+                g2.fillRect(platform.x, platform.y, platform.width, platform.height);
+            }
+            
+        }
+    }
+
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new EarthBoyWindGirl();
             }
         });
-    }
+    }    
 }
