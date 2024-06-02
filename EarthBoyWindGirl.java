f99b0d96589862987dcb421502fcdce347f6ec17
EmChen06 <emileechen95@gmail.com>
2024-06-02 13:50:38 -0400

    did stuff and now it broke

--- EarthBoyWindGirl.java
+++ EarthBoyWindGirl.java
@@ -6,58 +6,16 @@ import java.util.*;
 
 public class EarthBoyWindGirl extends JFrame {
 
-    DrawingPanel p;
+    DrawingPanel draw;
+    JPanel p;
     int WIDTH = 1000;
     int HEIGHT = 600;
 
     WindGirl windGirl;
     EarthBoy earthBoy;
 
-    ArrayList<Platform> platforms = new ArrayList<>();
-    
-    
-    EarthBoyWindGirl(){
-        this.setSize(new Dimension(WIDTH,HEIGHT));
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        
-        p = new DrawingPanel();
-        this.add(p);
-
-        
-        for (int i = 0; i < (WIDTH / 20); i++) {
-            for(int j = 0; j < (HEIGHT / 20); j++){
-                Platform p = new Platform(i * 20, j*20, 20, 20, null);
-                platforms.add(p);
-            }
-        }
-
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
-
-    }
-
-    private class DrawingPanel extends JPanel{
-
-        DrawingPanel(){
-            this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-
-            Graphics2D g2 = (Graphics2D)g;
-			//turn on antialiasing
-			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-
-            g2.setColor(Color.black);
-
-            for (Platform platform : platforms) {
-                g2.drawRect(platform.x, platform.y, platform.width, platform.height);
-                }
-            }
-            
-        }
+    // ArrayList<Platform> platforms = new ArrayList<>();
+    Platform plat;
 
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -65,5 +23,55 @@ public class EarthBoyWindGirl extends JFrame {
                 new EarthBoyWindGirl();
             }
         });
-    }    
+    } 
+    
+    EarthBoyWindGirl(){
+        this.setTitle("EarthBoy and WindGirl");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        p = new JPanel();
+        p.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+        draw = new DrawingPanel();
+
+        // plat = new Platform(0, 0, 100, 30);
+
+        // for (int i = 0; i < (WIDTH / 20); i++) {
+        //     for(int j = 0; j < (HEIGHT / 20); j++){
+        //         Platform p = new Platform(i * 20, j*20, 20, 20, null);
+        //         platforms.add(p);
+        //     }
+        // }
+
+        p.add(draw);
+        this.add(p);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+    class DrawingPanel extends JPanel{
+
+        DrawingPanel(){
+        }
+
+        @Override
+        public void paintComponent(Graphics g) { //changed to public instead of protected
+            super.paintComponent(g); 
+
+            Graphics2D g2 = (Graphics2D)g;
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON); //turn on antialiasing
+
+            g2.setColor(Color.black);
+            // g2.fillRect(plat.x, plat.y, plat.width, plat.height);
+
+            g2.fillRect(0,0,500,100);
+
+            // for (Platform platform : platforms) {
+            //     g2.drawRect(platform.x, platform.y, platform.width, platform.height);
+            //     }
+            // }
+            
+        }
+    }
 }
--- Platform.java
+++ Platform.java
@@ -3,8 +3,8 @@ import java.awt.image.BufferedImage;
 
 public class Platform extends Rectangle{
     BufferedImage image;
-    Platform(int x, int y, int width, int height, BufferedImage img){
+    Platform(int x, int y, int width, int height){ //TODO: add BufferedImage img into constructor
         super(x,y,width,height);
-        this.image = img;
+        // this.image = img;
     }
 }
