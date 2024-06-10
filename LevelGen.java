ca5dd2d3aa41961afffca531054ce5c436cc3fed
David <hangdavi0805@gmail.com>
2024-06-10 12:51:37 -0400

    DONT MIND THIS THIS IS A WIP

--- LevelGen.java
+++ LevelGen.java
@@ -1,36 +1,28 @@
+
 /**
  * Reads a .txt file to generate a level
  */
-import java.util.*;
 import java.io.*;
-import java.io.FileReader;		
-import java.io.BufferedReader;
-import java.util.*;
 
-public class LevelGen{
+public class LevelGen {
 
-    static int[][] f1;
+    static int[] f1;
+    int code;
 
     /*
      * Reads the file in which the "level" is stored in
      */
-    public static int[][] readFile(String name){
-        try{
-            
+    public static int[] readFile(String name) {
+        try {
+
             File file1 = new File(name);
             FileReader fin1 = new FileReader(file1);
             BufferedReader reader1 = new BufferedReader(fin1);
 
-            int lineNum = 0;
             String x = reader1.readLine();
-            while(x != null){
+            while (x != null) {
                 x = reader1.readLine();
-                lineNum += 1;
             }
-
-            
-
-            
 
             reader1.close();
             fin1.close();
@@ -39,20 +31,17 @@ public class LevelGen{
             reader1 = new BufferedReader(fin1);
 
             String count1 = reader1.readLine();
-            f1 = new int[lineNum][count1.length()];
+            f1 = new int[count1.length()];
 
-            for (int i = 0; i < lineNum; i++) {
-                for (int j = 0; j < count1.length(); j++) {
-                    f1[i][j] = Integer.parseInt(String.valueOf(count1.charAt(j)));
-                }
+            for (int i = 0; i < count1.length(); i++) {
+                f1[i] = Integer.parseInt(String.valueOf(i));
                 count1 = reader1.readLine();
             }
-
 
             reader1.close();
             fin1.close();
 
-        } catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("Error - File Not Found, please enter a valid file name \n");
         } catch (IOException e) {
             System.out.println("Error");
@@ -62,11 +51,10 @@ public class LevelGen{
     }
 
     public static void main(String[] args) {
-        int[][] x = readFile("test.txt");
+        int[] x = readFile("test.txt");
 
-        for (int[] is : x) {
-            System.out.println(Arrays.toString(is));
-        }
-        
+        // for (int[] is : x) {
+        //   System.out.println(Arrays.toString(is));
+        //}
     }
 }
