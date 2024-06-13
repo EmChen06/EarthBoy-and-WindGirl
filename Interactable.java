447698ceeb2aa9e5d1ebb533a0edd38cb274cad8
Sheng-777 <hanshengliu2024@gmail.com>
2024-06-13 11:18:52 -0400

    Finish comment for door, interactable, and PressurePlate

--- Interactable.java
+++ Interactable.java
@@ -1,3 +1,10 @@
+/**
+ * Interactable.java
+ * Emilee Chen, David Hang, Hansheng Liu
+ * June 13th, 2024
+ * Interactable class which is an abstract class for all interactables
+ */
+
 import java.awt.image.BufferedImage;
 
 public abstract class Interactable extends Platform{
@@ -5,11 +12,23 @@ public abstract class Interactable extends Platform{
     BufferedImage img;
     boolean interacted = false;
 
+    /**
+     * Constructor for interactables
+     * @param x top-left x-coordinate
+     * @param y top-left y-coordinate
+     * @param w width of interactable
+     * @param h height of interactable
+     * @param image image of interactable
+     */
     Interactable(int x, int y, int w, int h, BufferedImage image) {
         super(x, y, w, h);
         this.img = image;
     }
 
+    /**
+     * Check collision with character, if colliding perform onInteraction
+     * @param c the character to check collision with
+     */
     void checkCollision(Character c){
         interacted = colliding(c);
 
@@ -22,6 +41,11 @@ public abstract class Interactable extends Platform{
         }
     }
 
+    /**
+     * Check collision with two player, useful for Door class where both player need to be on
+     * @param c1    the first player
+     * @param c2    the second player
+     */
     void checkCollision(Character c1, Character c2){
         interacted = (colliding(c1) || colliding(c2));
         // System.out.println(interacted);
@@ -34,6 +58,11 @@ public abstract class Interactable extends Platform{
         }
     }
 
+    /**
+     * Define collision for character and interactable
+     * @param c the character to check collision for
+     * @return  whether there is a collision
+     */
     boolean colliding(Character c){
         
         if (c.x < this.x+this.width && c.x  + c.w> this.x && c.y + c.h>= this.y && c.vy < 0 && c.y + c.h <= this.y+this.height && c.y + c.h + c.vy <= this.y) {
@@ -59,7 +88,15 @@ public abstract class Interactable extends Platform{
     } 
     }
 
+    /**
+     * abstract method, what an interactable do on collision
+     * @param c the character interacting with the interactable
+     */
     abstract void onInteraction(Character c);
 
+    /**
+     * abstract method, what an interactable do when player leaves
+     * @param c the character interacting with the interactable
+     */
     abstract void onLeave(Character c);
 }
--- PressurePlate.java
+++ PressurePlate.java
@@ -1,21 +1,45 @@
+/**
+ * PressurePlate.java
+ * Emilee Chen, David Hang, Hansheng Liu
+ * June 13th, 2024
+ * Pressure plate class which move a platform once stand on it
+ */
+
 import java.awt.image.BufferedImage;
 
 public class PressurePlate extends Interactable{
 
     Platform p;
 
+    /**
+     * Constructor for platform class
+     * @param x top-left x-coordinate
+     * @param y top-right y-coordinate
+     * @param w width of pressureplate
+     * @param h height of pressureplate
+     * @param image image of pressureplate
+     * @param p platform that the pressure plate control
+     */
     PressurePlate(int x, int y, int w, int h, BufferedImage image, Platform p) {
         super(x, y, w, h, image);
         this.p = p;
     }
 
     @Override
+    /**
+     * when interacting with pressureplate, move the corresponding platform
+     * @param c the character standing on the pressure plate
+     */
     void onInteraction(Character c) {
         //System.out.println("enter");
         p.move();
     }
 
     @Override
+    /**
+     * if player leave pressureplate, platform moves back
+     * @param c the character leaving the pressure plate
+     */
     void onLeave(Character c) {       
         
         p.move(p.vx * -1, p.vy * -1);
