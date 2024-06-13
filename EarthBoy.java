56672efbab38e54761698ca6fae871dd9611f24b
DregoonXD <hangdavi0805@gmail.com>
2024-06-13 11:20:11 -0400

    Added Comments to Windgirl and Earthboy classes

--- EarthBoy.java
+++ EarthBoy.java
@@ -2,20 +2,35 @@ import java.awt.image.BufferedImage;
 
 public class EarthBoy extends Character{
 
+    //initialize variables
     boolean isBuild;
     boolean readyBuild = true;
 
+    /**
+     * Creates an Earthboy Object
+     * @param startX    the starting x position of Earthboy
+     * @param startY    the startion y position of Earthboy
+     * @param image     Earthboy's Image
+     * @param jump      Checking if he is jumping
+     * @param dead      Checking if he is dead
+     * @param build     Checking if he is building
+     */
     EarthBoy(int startX, int startY, BufferedImage image, boolean jump, boolean dead, boolean build) {
         super(startX, startY, image, jump, dead);
         this.isBuild = build;
         this.name = "earthBoy";
     }
 
+    /**
+     * allows Earthboy to enter build mode
+     */
     void enterBuildMode() {
         this.isBuild = true;
     }
 
-    //it wont move
+    /**
+     * wont move unless he is not in build mode
+     */
     @Override
     void move() {
         if (! this.isBuild){
@@ -23,10 +38,16 @@ public class EarthBoy extends Character{
         } 
     }
 
+    /*
+     * Leaves the build mode
+     */
     void leaveBuildMode() {
         this.isBuild = false;
     }
 
+    /*
+     * Checking if he is in build mode or not
+     */
     public void setReadyBuild(boolean readyBuild) {
         this.readyBuild = readyBuild;
     }
--- WindGirl.java
+++ WindGirl.java
@@ -1,13 +1,26 @@
 import java.awt.image.BufferedImage;
 
 public class WindGirl extends Character{
+   
+    //initialize variables
     boolean isDoubleJump = false;
-
+    
+    /**
+     * Creates a wind girl object
+     * @param startX    the starting x position of Windgirl
+     * @param startY    the startion y position of Windgirl
+     * @param image     Windgirl's Image
+     * @param jump      Checking if she is jumping
+     * @param dead      Checking if she is dead
+     */
     WindGirl(int startX, int startY, BufferedImage image, boolean jump, boolean dead) {
         super(startX, startY, image, jump, dead);
         this.name = "windGirl";
     }
 
+    /**
+     * Enables windgirl's drifting mechanic as well as her regular movement
+     */
     @Override
     void move() {
         if (this.vy < 0 && !this.preparedJump){
@@ -20,6 +33,10 @@ public class WindGirl extends Character{
         super.move();
     }
 
+    /**
+     * Checks if she is double jumping or not
+     * @param doubleJump the boolean if doublejump is enabled or not
+     */
     void setDoubleJump(boolean doubleJump) {
         this.isDoubleJump = doubleJump;
     }
